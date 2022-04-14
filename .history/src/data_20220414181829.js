0488db082218a05c9e5d48a16510ffea0cec457d
Bodescu Anamaria <bodescu.anamariia@gmail.com>
2022-04-14 18:21:00 +0300

    feat: add people in Data

--- .history/src/data_20220414181829.js
+++ .history/src/data_20220414181829.js
@@ -2,21 +2,21 @@ export default [
   {
     id: 1,
     name: "Bertie Yates",
-    dateOfBirth: "04/14/1996",
+    dateOfBirth: "01/05/1996",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
   },
   {
     id: 2,
     name: "Hester Hogan",
-    dateOfBirth: "04/14/1988",
+    dateOfBirth: "02/16/1988",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
   },
   {
     id: 3,
     name: "Larry Little",
-    dateOfBirth: "04/14/2001",
+    dateOfBirth: "03/09/2001",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
   },
@@ -30,56 +30,56 @@ export default [
   {
     id: 5,
     name: "Lola Gardner",
-    dateOfBirth: "04/15/1994",
+    dateOfBirth: "05/30/1994",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
   },
   {
     id: 6,
     name: "Bertie Yates",
-    dateOfBirth: "04/15/1996",
+    dateOfBirth: "01/05/1996",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
   },
   {
     id: 7,
     name: "Hester Hogan",
-    dateOfBirth: "04/15/1988",
+    dateOfBirth: "02/16/1988",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
   },
   {
     id: 8,
     name: "Larry Little",
-    dateOfBirth: "04/16/2001",
+    dateOfBirth: "03/09/2001",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
   },
   {
     id: 9,
     name: "Sean Walsh",
-    dateOfBirth: "04/16/1999",
+    dateOfBirth: "04/14/1999",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
   },
   {
     id: 10,
     name: "Lola Gardner",
-    dateOfBirth: "04/16/1994",
+    dateOfBirth: "05/30/1994",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
   },
   {
     id: 11,
     name: "Bertie Yates",
-    dateOfBirth: "04/16/1996",
+    dateOfBirth: "01/05/1996",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
   },
   {
     id: 12,
     name: "Hester Hogan",
-    dateOfBirth: "04/16/1988",
+    dateOfBirth: "02/16/1988",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
   },
