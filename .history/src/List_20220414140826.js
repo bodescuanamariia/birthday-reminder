28cdc387d7000097d1a480ea03bf207257b9d20b
Bodescu Anamaria <bodescu.anamariia@gmail.com>
2022-04-14 14:09:51 +0300

    create list component

---
+++ .history/src/List_20220414140826.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const List = () => {
+  return;
+  <>
+    <h2>lis component</h2>
+  </>;
+};
---
+++ .history/src/List_20220414140912.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const List = () => {
+  return (
+    <>
+      <h2>lis component</h2>
+    </>
+  );
+};
+
+export default List;
